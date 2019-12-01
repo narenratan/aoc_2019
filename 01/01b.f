fad744c86583e84faeffd09d110cc7a47ae74e79
Naren Ratan <narenratan@gmail.com>
2019-12-01 22:32:23 +0000

    Day 1 b in Forth

---
+++ 01/01b.f
@@ -0,0 +1,8 @@
+∇ READONE / a→na / 0 ↕ ( ↑ C@ ↑ 0A ≠ ∥ 30 - ⌽ A × + ↕ 1+ ) ↓ 1+ ∇  / Read next decimal number /
+∇ FUEL 3 ÷ 2 - ∇
+∇ +FUEL / nn → nn / FUEL ↑ ⌽ + ↕ ∇
+∇ FUELTOT / n → n / FUEL ↑ ( ↑ 0> ∥ +FUEL ) - ∇
+∇ ALLFUEL / na→n / ( READONE -⌽ FUELTOT + ↕ ↑ C@ ∥) ↓ ∇ / Add fuel until next byte is zero /
+
+0 H" input.txt" ↓ ⍐ 1000 H ⌽ ⍇ H + 0 ↕ ! / Read file and put zero byte at the end /
+0 H ALLFUEL .
